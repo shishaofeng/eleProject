8298aa003402f3b32711784ec365f10e0ae84f62
shishaofeng <18839966905@163.com>
2021-12-18 08:39:10 +0800

    发布npm包

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const webpack = require("webpack");
+const nodeEnv = process.env.nodeEnv;
+module.exports = {
+  publicPath: "/",
+  productionSourceMap: false,
+  outputDir: "./dist/eleProjet.js",
+};
