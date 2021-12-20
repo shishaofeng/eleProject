9ddbb06e3e5fe664fd2de0040a6b0d8300b1d697
shishaofeng <1246683986@qq.com>
2021-12-20 16:53:48 +0800

    按钮组件JSON配置开发

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
-import common from "../lib/index";
-// import vueEleProject from "vue-ele-project";
+// import common from "../lib/index";
+import vueEleProject from "vue-ele-project";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
-// Vue.use(vueEleProject);
-Vue.use(common);
+Vue.use(vueEleProject);
+// Vue.use(common);
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
